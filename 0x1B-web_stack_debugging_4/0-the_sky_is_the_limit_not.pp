e294841458b65e8b9d2231b52bde99e5111e9086
MarySerna <mary.serna.8903@gmail.com>
2020-05-28 22:45:57 +0000

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # fix error
 exec {'increase':
-    command => 'sudo sed -i "s/worker_processes 4;/worker_processes 20;worker_rlimit_nofile 3000;/" /etc/nginx/nginx.conf; sudo service nginx restart',
-# lint:ignore:140chars
+    command => 'sudo sed -i "s/worker_processes 4;/worker_processes 20;worker_rlimit_nofile 3000;/" /etc/nginx/nginx.conf; sudo service nginx restart', # lint:ignore:140chars
     path    => '/etc/nginx/nginx.conf:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
